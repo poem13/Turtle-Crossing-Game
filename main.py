d9b11ce5545fb11b337d63093c1fe0cc09058a11
Poe <poem1300@gmail.com>
2022-05-01 11:29:10 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+turtle = Player()
+car_manager = CarManager()
+speed = 10
+
+screen.listen()
+screen.onkeypress(turtle.go_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+    turtle.is_top()
+
+    for car in car_manager.all_cars:
+        if car.distance(turtle) < 20:
+            game_is_on = False
+        if turtle.is_top():
+            car.backwards += speed
+
+screen.exitonclick()
---
+++ player.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.tilt(90)
+
+    def go_up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def is_top(self):
+        if self.ycor() >= FINISH_LINE_Y:
+            self.goto(STARTING_POSITION)
